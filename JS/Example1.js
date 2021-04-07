1b4ff0d44369cae254c06addc857cef541708dc3
thantai01 <thantaihp2011@gmail.com>
2021-04-07 16:56:02 +0700

    JS ngay 1

---
+++ JS/Example1.js
@@ -0,0 +1,14 @@
+// output
+    //alert()
+    //console.log()
+    //confirm()
+   // document.getElementById().innerText
+    //document.write()
+// input box on js
+   // prompt('mess',)
+//variable
+    //let =+prompt('Nummber X='+x)
+    //let =+prompt('Nummber Y='+y)
+    //z=x+y
+    //alert('Result='+z)
+//
